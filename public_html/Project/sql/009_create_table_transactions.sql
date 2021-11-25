8f107c54595c240fa1a855d4096daa07c8a2b0a4
karnveer32 <karnveer32@gmail.com>
2021-11-25 14:12:28 -0500

    Added Transactions Table

---
+++ public_html/Project/sql/009_create_table_transactions.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS `Transactions`
+(
+    `id` int auto_increment PRIMARY KEY,
+    `account_number` varchar(12) unique,
+    `routing_number` varchar(12) unique, 
+    `user_id` int,
+    `balance` bigint default 0,
+    `created` timestamp default current_timestamp,
+    `modified` timestamp default current_timestamp on update current_timestamp,
+    `account_type` varchar(20),
+    FOREIGN KEY (`user_id`) REFERENCES Users(`id`),
+    check (LENGTH(`account_number`) = 12)
+)
