81032758247943957af391c7f33885d2af986b9e
karnveer32 <karnveer32@gmail.com>
2021-11-27 17:10:29 -0500

    Created file

---
+++ public_html/Project/accounts.php
@@ -0,0 +1,17 @@
+<?php
+//Here I want to create a page will show all my accounts 
+//redirect from createAccounts.php
+//max 5 accounts
+//after createaccounts.php code is run, i want to display each accounts: account_number, account_type, & balance
+?>
+
+<h1>My Accounts</h1>
+<nav>
+    <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+</nav>
