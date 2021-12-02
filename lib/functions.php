d176982d13095b1ac9392779e58dc0e3a0258899
karnveer32 <karnveer32@gmail.com>
2021-12-02 14:04:08 -0500

    Successful completion of issue

--- lib/functions.php
+++ lib/functions.php
@@ -195,6 +195,72 @@ function get_or_create_account()
     }
 }
 
+
+function get_or_create_account2()
+{
+    if (is_logged_in()) {
+        //let's define our data structure first
+        //id is for internal references, account_number is user facing info, and balance will be a cached value of activity
+        $account = ["id" => -1, "account_num" => false, "balance" => 0];
+        //this should always be 0 or 1, but being safe
+        $query = "SELECT id, account_number, balance from Accounts where user_id = :uid LIMIT 1";
+        $db = getDB();
+        $stmt = $db->prepare($query);
+        try {
+            $stmt->execute([":uid" => get_user_id()]);
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+            if (!$result) {
+                //account doesn't exist, create it
+                $created = false;
+                //we're going to loop here in the off chance that there's a duplicate
+                //it shouldn't be too likely to occur with a length of 12, but it's still worth handling such a scenario
+
+                //you only need to prepare once
+                $query = "INSERT INTO Accounts (account_number, user_id) VALUES (:an, :uid)";
+                $stmt = $db->prepare($query);
+                $user_id = get_user_id(); //caching a reference
+                $account_number = "";
+                while (!$created) {
+                    try {
+                        $account_number = (mt_rand(000000000000, 999999999999));
+                        $stmt->execute([":an" => $account_number, ":uid" => $user_id]);
+                        $created = true; //if we got here it was a success, let's exit
+                        flash("Welcome! Your account has been created successfully", "success");
+                    } catch (PDOException $e) {
+                        $code = se($e->errorInfo, 0, "00000", false);
+                        //if it's a duplicate error, just let the loop happen
+                        //otherwise throw the error since it's likely something looping won't resolve
+                        //and we don't want to get stuck here forever
+                        if (
+                            $code !== "23000"
+                        ) {
+                            throw $e;
+                        }
+                    }
+                }
+                //loop exited, let's assign the new values
+                $account["id"] = $db->lastInsertId();
+                $account["account_num"] = $account_number;
+            } else {
+                //$account = $result; //just copy it over
+                $account["id"] = $result["id"];
+                $account["account_num"] = $result["account_number"];
+                $account["balance"] = $result["balance"];
+                //return $result["id"];
+                //return $result["account_number"];
+                return $result["balance"];
+            }
+        } catch (PDOException $e) {
+            flash("Technical error: " . var_export($e->errorInfo, true), "danger");
+        }
+        $_SESSION["user"]["account_number"] = $account; //storing the account info as a key under the user session
+        //Note: if there's an error it'll initialize to the "empty" definition around line 161
+
+    } else {
+        flash("You're not logged in", "danger");
+    }
+}
+
 function get_account_balance()
 {
     if (is_logged_in() && isset($_SESSION["user_id"]["account_number"])) {
--- public_html/Project/transactionsHistory.php
+++ public_html/Project/transactionsHistory.php
@@ -1,17 +1,46 @@
 <?php
 require(__DIR__ . "/../../partials/nav.php");
 $a = get_or_create_account();
-$b = get_account_balance();
-$c = get_account_date();
+$b = get_or_create_account2();
+$results=[];
+$results2=[];
+$src=se($_GET, "id", -1, false);
+$stmt = $db->prepare("SELECT id, src, dest, diff FROM Transactions WHERE src =:src");
+try{
+    $stmt -> execute([":src" => $src]);
+    $r = $stmt->fetchALL(PDO::FETCH_ASSOC);
+        if ($r) {
+            
+            $results = $r;
+        }
+    }
+    catch(PDOException $e){
+        flash("<pre>" . var_export($e, true). "</pre>");
+    }
+$time="Data";
+
+$stmt2=$db->prepare("SELECT created FROM Accounts WHERE id=:src");
+try{
+    $stmt2 -> execute([":src" => $src]);
+    $r2 = $stmt2->fetchALL(PDO::FETCH_ASSOC);
+        if ($r2) {
+            
+            $results2 = $r2;
+        }
+    }
+    catch(PDOException $e){
+        flash("<pre>" . var_export($e, true). "</pre>");
+    }
 ?>
 
+<div class="container-fluid">
 <h1> Account Information </h1>
 <nav>
     <ul>
         <li> Account Number: <?php echo $a; ?> </li>
         <li> Account Type: Checking </li>
-        <li> Balance: <?php echo $b; ?></li>
-        <li> Opened/Created Date: <?php echo $c; ?></li>
+        <li> Balance: $<?php echo $b; ?></li>
+        <li> Opened/Created Date: <?php foreach ($results2 as $result) : se($result, 'created');  endforeach; ?> </li>
         <li> Transaction History: </li>
         <br>
         <div>
@@ -22,21 +51,41 @@ $c = get_account_date();
                     <th>Destination</th>
                     <th>Difference</th>
                 </thead>
+                <tbody> 
+                    <?php if(!$results || count($results)==0) : ?>
+                    <tr>
+                            <td colspan="100%"> No <?php se($time); ?> to display</td>
+                        </tr>
+                    <?php else:?>
+                        <?php foreach ($results as $result) : ?>
+                            <tr>
+                                <td>
+                                    <?php se($result, 'id'); ?><?php //se($result, "src"); ?><?php //se($result, "dest"); ?><?php //se($result, "diff"); ?></a>
+                                    <?php //se($result, "id"); ?>
+                                </td>
+                                <td><?php se($result, "src"); ?></td>
+                                <td><?php se($result, "dest"); ?></td>
+                                <td><?php se($result, "diff"); ?></td>
+                            </tr>
+                        <?php endforeach; ?>
+                    <?php endif; ?>
             </table>
         </div>
     </ul>
 </nav>
 
 <?php
+/*
 $conn = mysqli_connect("localhost", "root", "", "Transactions");
 if($conn -> connect_error) {
     die("connection failed:". $conn -> connect_error);
 }
+*/
 
-$sql = "SELECT id, src, dest, diff FROM Transactions";
-$result = $conn  -> query($sql);
+//$sql = "SELECT id, src, dest, diff FROM Transactions WHERE user_id =:uid"
+//$result = $conn  -> query($sql);
 
-if($result -> num_rows > 0) {
+/*if($result -> num_rows > 0) {
     while($row = $result -> fetch_assoc()){
         echo "<tr><td>". $row["id"] . "</td><td>" . $row["src"] . "</td><td>" . $row["dest"] . "</td><td>" . $row["diff"] . "</td></tr>";
     }
@@ -45,5 +94,6 @@ if($result -> num_rows > 0) {
 else {
     echo "0 result";
 }
-
 $conn -> close();
+*/
+
