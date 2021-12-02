5a5b02476d7aadaa09c05d0ad621971cb47aefe5
karnveer32 <karnveer32@gmail.com>
2021-12-02 12:00:11 -0500

    Created Accounts List Page

--- public_html/Project/accounts.php
+++ public_html/Project/accounts.php
@@ -9,23 +9,39 @@ require_once(__DIR__ . "/../../lib/functions.php");
 require_once(__DIR__ . "/../../partials/flash.php");
 
 $pass = get_or_create_account();
+$user_id=get_user_id();
 $balancepass = get_or_create_account2();
+
+$stmt = $db->prepare("SELECT account_number, balance FROM Accounts WHERE user_id = :uid LIMIT 5");
+$result =[];
+try{
+$stmt -> execute([":uid" => $user_id]);
+$r = $stmt->fetchALL(PDO::FETCH_ASSOC);
+    if ($r) {
+        
+        $result = $r;
+    }
+}
+catch(PDOException $e){
+    flash("<pre>" . var_export($e, true). "</pre>");
+}
+
+
 ?>
 
-
+<div class="container-fluid">
 <h1>My Accounts</h1>
+<?php foreach ($result as $item) : ?>
 <nav>
     <ul>
-        <li>Account #1:
+        <li> <a href="transactionsHistory.php"> Account:</a>
             <ul>
-                <li> Account Number: <?php echo $pass; ?> </li>
+                <li> Account Number: <?php se($item, "account_number"); ?> </li>
                 <li> Account Type: Checking </li>
-                <li> Balance: $<?php echo $balancepass; ?></li>
+                <li> Balance: $<?php se($item, "balance"); ?></li>
             </ul>
         </li> 
-        <li>Account #2: </li>
-        <li>Account #3: </li>
-        <li>Account #4: </li>
-        <li>Account #5: </li>
     </ul>
 </nav>
+<?php endforeach; ?>
+</div>
